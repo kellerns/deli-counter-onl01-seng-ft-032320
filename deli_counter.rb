c7182ea9f068fd572e79e2476a15e0f7b3d1d306
Nicholas Keller <nic.keller328@gmail.com>
2020-03-26 19:18:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,22 +6,22 @@ def line(katz_deli)
   end
 end
 
-# def take_a_number(katz_deli, name)
-#   katz_deli << name
-#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-#   new_line = "#{katz_deli.length}. #{name}"
-# end
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  new_line = "#{katz_deli.length}. #{name}"
+end
 
-# def now_serving(katz_deli)
-#   if katz_deli.length == 0
-#     puts "There is nobody waiting to be served!"
-#   else
-#     puts "Currently serving #{katz_deli[0]}."
-#     katz_deli.shift
-#   end
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
 
 
-# end
+end
   
   
   
