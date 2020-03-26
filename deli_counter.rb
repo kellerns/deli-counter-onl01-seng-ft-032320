aa11099e1e47ff6e16c838ddfe4e9ab32d2d404c
Nicholas Keller <nic.keller328@gmail.com>
2020-03-26 19:05:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  line_order = "#{katz_deli.length}. #{name}"
+  if katz_deli.length == 0
+    "The line is currently empty."
+  else
+    puts "The line is currently:" + 
+  end
+  new_line_array = 
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  new_line = "#{katz_deli.length}. #{name}"
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+
+
+end
+  
+  
+  
+  
